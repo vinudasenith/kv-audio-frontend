9102970c59bfde2f3a506a87c160efc7fb81434c
vinudasenith <ha.vinudas@gmail.com>
2025-04-10 18:52:12 +0530

    init 💥

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+  return (
+    <div>
+      <ProductCard name="Sound Mixer HI-L003" price="15,650" description="Ideal for music production, live performances" photoURL="https://tse4.mm.bing.net/th?id=OIP.WRdQpB0GpvZcsfKTG9XcRgHaEK&pid=Api&P=0&h=220" />
+
+      <ProductCard name="Head Sst Bluetooth 2025 JBL" price="15,650" description="Ideal for music production, live performances" photoURL="https://img.drz.lazcdn.com/static/lk/p/bbe9b7889dfd6581c37ecc1d030e1e23.jpg_720x720q80.jpg_.webp" />
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,12 @@
+import "./productCard.css"
+export default function ProductCard(props) {
+    console.log(props)
+    return (
+        <div>
+            <img src={props.photoURL} alt={props.name} />
+            <span>{props.name}</span>
+            <span>LKR {props.price}</span>
+            <p>{props.description}</p>
+        </div>
+    )
+}
