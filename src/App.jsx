7c5776265f72718f50f26a5363b960cd7c55bb92
vinudasenith <ha.vinudas@gmail.com>
2025-04-16 08:21:53 +0530

    started to mke adminPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,16 @@ import { ImParagraphJustify } from "react-icons/im";
 import { BsFillSaveFill } from "react-icons/bs";
 import { RiAlignItemBottomLine } from "react-icons/ri";
 import { FaUser } from "react-icons/fa";
+import AdminPage from './components/adminPage';
+
 
 function App() {
   return (
-    <div className="w-full h-screen flex">
-      <div className="w-[400px] h-full bg-green-500">
-        <button className="w-full h-[40px] text-[20px] font-bold  flex justify-center items-center"><ImParagraphJustify />Dashboard</button>
-        <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center"><BsFillSaveFill /> Bookings</button>
-        <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center "><RiAlignItemBottomLine /> Items</button>
-        <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center "><FaUser />Users</button>
-      </div>
-
-      <div className="w-full bg-red-900">
-        <MdOutlineBluetoothAudio className="text-[300px] text-white" />
-      </div>
-
+    <div>
+      <AdminPage />
 
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/adminPage.jsx
@@ -0,0 +1,22 @@
+
+import { ImParagraphJustify } from "react-icons/im";
+import { BsFillSaveFill } from "react-icons/bs";
+import { RiAlignItemBottomLine } from "react-icons/ri";
+import { FaUser } from "react-icons/fa";
+export default function AdminPage() {
+    return (
+        <div className="w-full h-screen flex">
+            <div className="w-[400px] h-full bg-green-500">
+                <button className="w-full h-[40px] text-[20px] font-bold  flex justify-center items-center"><ImParagraphJustify />Dashboard</button>
+                <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center"><BsFillSaveFill /> Bookings</button>
+                <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center "><RiAlignItemBottomLine /> Items</button>
+                <button className="w-full h-[40px] text-[25px] font-bold  flex justify-center "><FaUser />Users</button>
+            </div>
+
+            <div className="w-full bg-red-900">
+            </div>
+
+
+        </div>
+    )
+}
