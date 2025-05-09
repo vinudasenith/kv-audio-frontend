f0493f66865b48584c496ec6547bd3cbae6b6a4f
vinudasenith <ha.vinudas@gmail.com>
2025-05-09 23:46:00 +0530

    implement booking item page,booking page,product overview page,admin user page,admin page✅

--- src/pages/home/productOverview.jsx
+++ src/pages/home/productOverview.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ImageSlider from "../../components/imageSlider";
+import { addToCart, loadCart } from "../../utils/cart";
 export default function ProductOverview() {
 
     const params = useParams();
@@ -33,8 +34,11 @@ export default function ProductOverview() {
             {
                 loadingStatus == "loaded" &&
                 <div className="w-full h-full flex justify-center items-center">
-                    <div className="w-[49%] h-full"><ImageSlider images={product.image} /></div>
-                    <div className="w-[49%] h-full flex flex-col items-center ">
+                    <div className="w-[49%] h-full">
+                        <ImageSlider images={product.image} />
+                    </div>
+
+                    <div className="w-[49%] h-full flex flex-col items-center">
                         <h1 className="text-3xl font-bold text-accent">{product.name}</h1>
                         <h2 className="text-3xl font-bold text-accent">{product.category}</h2>
                         <p className="text-lg font-bold text-accent">{product.description}</p>
@@ -42,9 +46,20 @@ export default function ProductOverview() {
                         <p className="text-lg font-bold text-accent">Rs. {product.price}</p>
 
 
-
+                        <button
+                            className="w-[200px] h-[50px] bg-blue-500 text-white py-2 mt-4 rounded-lg hover:bg-blue-600"
+                            onClick={() => {
+                                addToCart(product.key, 1);
+                                console.log(loadCart());
+                                window.location.href = "/booking";
+                            }}
+                        >
+                            Add to Cart
+                        </button>
                     </div>
                 </div>
+
+
             }
             {
                 loadingStatus == "error" &&
